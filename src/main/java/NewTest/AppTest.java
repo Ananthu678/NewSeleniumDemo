38ca4e886d2e661fa449f07a73671f1cb4a6cf57
Ananthu678 <76966844+Ananthu678@users.noreply.github.com>
2021-01-08 19:49:24 +0530

    uploadingSelenium test

---
+++ src/main/java/NewTest/AppTest.java
@@ -0,0 +1,31 @@
+package NewTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\I322911\\eclipse-workspace\\NewSeleniumDemo\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get("http://18.220.170.152:8888/");
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        
+        driver.findElement(By.className("v-button")).click();
+        driver.findElement(By.id("gwt-uid-5")).sendKeys("Ananthu36");
+        driver.findElement(By.id("gwt-uid-7")).sendKeys("Edureka1");
+        driver.findElement(By.id("gwt-uid-9")).sendKeys("9894828235");
+        driver.findElement(By.id("gwt-uid-11")).sendKeys("ananthu.siva05@gmail.com");
+        driver.findElement(By.id("gwt-uid-13")).sendKeys("01/01/2021");
+		driver.findElement(By.className("v-button-primary")).click();
+
+	}
+
+}
